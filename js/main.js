2bfb4ebfaac692ef36e9e00673e35482df57ec8a
Paul Vegan <buurzuum@gmail.com>
2018-01-31 00:24:17 +0000

    working on responsives

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
 $(document).ready(function() {
+  // open overlay navigation bar
   $('#toggle').click(function() {
     $(this).toggleClass('active');
+    $('#overlay').toggleClass('open');
+  });
+  // back to home
+  $("#home-button").click(function() {
+    $('body,html').animate({
+      scrollTop: 0 // Scroll to top of body
+    }, 500);
+    $('#overlay').toggleClass('open');
+  });
+  // scroll to about section
+  $("#about-button").click(function() {
+    $('html, body').animate({
+      scrollTop: $(".about").offset().top
+    }, 1000);
+    $('#overlay').toggleClass('open');
+  });
+  // scroll to works section
+  $("#works-button").click(function() {
+    $('html, body').animate({
+      scrollTop: $('.works').offset().top
+    }, 1000);
+    $('#overlay').toggleClass('open');
+  });
+  // scroll to contact-form section
+  $("#contact-button").click(function() {
+    $('html, body').animate({
+      scrollTop: $('.contact').offset().top
+    }, 1000);
     $('#overlay').toggleClass('open');
   });
 });
